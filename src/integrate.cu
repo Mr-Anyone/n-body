997bdc3f8db468d4a5e0e665b97a2f8d4589f2c8
Vincent <llvm@viceroygroup.ca>
2025-09-18 12:54:25 -0700

    allocate and transfer memory from host to device and device to host

--- src/integrate.cu
+++ src/integrate.cu
@@ -28,13 +28,33 @@ void integrate_impl(Body* next_state, Body* current_state, int size,
     next_state[current_index].pos = current_state[current_index].pos + current_state[current_index].vel * time_delta;
 }
 
-void integrate(Body* next_state, Body* current_state, int size,
+// FIXME: next_state, and current_state are from host, 
+// we need to perform a memcpy 
+void integrate(Body* next_state, Body* current_state, int count, 
         float time_delta){
+    Body* next_state_cuda;
+    Body* current_state_cuda;
+    cudaMalloc(&next_state_cuda, count * sizeof(Body));
+    cudaMemcpy(next_state_cuda, next_state,
+            sizeof(Body)*count, cudaMemcpyHostToDevice);
+
+    cudaMalloc(&current_state_cuda, count * sizeof(Body));
+    cudaMemcpy(current_state_cuda, current_state,
+            sizeof(Body)*count, cudaMemcpyHostToDevice);
+
     // FIXME: we are taking the floor, so there are some objects 
     // that aren't being executed into the buffer
-    int block_number = size  / 512;
+    int block_number =  count / 512;
     // one block and 512 threads, change this later to respect size
-    integrate_impl<<<block_number, 512>>>(next_state, current_state, size, time_delta);
+    integrate_impl<<<block_number, 512>>>(next_state, current_state, count, time_delta);
     cudaDeviceSynchronize();
+
+    cudaMemcpy(next_state, next_state_cuda,
+            sizeof(Body)*count, cudaMemcpyDeviceToHost);
+    // FIXME: I don't think we need to copy current state into host device
+    cudaMemcpy(current_state, current_state_cuda,
+            sizeof(Body)*count, cudaMemcpyDeviceToHost);
+    cudaFree(current_state_cuda);
+    cudaFree(next_state_cuda);
 }
 
